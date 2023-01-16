66e9fb52fac2732b49e2696b77d29cd7d16ce61c
brrisso <bruno.risso1801@alumnos.ubiobio.cl>
2023-01-16 20:50:18 -0300

    Controller Centro y medicamento

--- projectapp/app/Http/Controllers/CentroController.php
+++ projectapp/app/Http/Controllers/CentroController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class CentroController extends Controller
 {
-    
+    protected CentroRepository $inteRepo;
+    public function __construct(CentroRepository $inteRepo)
+    {
+        $this->inteRepo = $inteRepo;
+    }
+
+    public function registrarCentro(Request $request)
+    {
+        return $this->inteRepo->registrarCentro($request);
+    }
+
+    public function eliminarCentro(Request $request)
+    {
+        return $this->inteRepo->eliminarCentro($request);
+    }
 }
--- projectapp/app/Http/Controllers/MedicamentoController.php
+++ projectapp/app/Http/Controllers/MedicamentoController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\CentroRepository;
+use App\Repositories\MedicamentoRepository;
 use Illuminate\Http\Request;
 
 class MedicamentoController extends Controller
 {
-    
+    protected MedicamentoRepository $inteRepo;
+    public function __construct(MedicamentoRepository $inteRepo)
+    {
+        $this->inteRepo = $inteRepo;
+    }
+
+    public function registrarMedicamento(Request $request)
+    {
+        return $this->inteRepo->registrarMedicamento($request);
+    }
+
+    public function eliminarMedicamento(Request $request)
+    {
+        return $this->inteRepo->eliminarMedicamento($request);
+    }
 }
