87c10ef66ad813f777c01f2155d13e2630e9d511
brrisso <bruno.risso1801@alumnos.ubiobio.cl>
2023-01-17 00:30:42 -0300

    traspaso repository

--- projectapp/app/Repositories/TraspasoRepository.php
+++ projectapp/app/Repositories/TraspasoRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\Traspaso;
+use App\Models\Centro_Distribucion;
 
 use Exception;
 use Illuminate\Http\Response;
@@ -10,5 +11,24 @@ use Illuminate\Support\Facades\Log;
 
 class TraspasoRepository
 {
-    
+    public function registroEgreso($request)
+    {
+        try
+        {
+            $cd_origen = Centro_Distribucion::select('id')->where('id', $request->id_cd)->get();
+            $cd_destino = Traspaso::select('id')->where('id', $request->id_cd)->get();
+
+            $egreso = Traspaso::create([
+                'tras_cd_destino' => $cd_destino,
+                'tras_cd_origen' => $cd_origen,
+                'tras_estado' => $request->tras_estado,
+            ]);
+
+            return response()->json(["mensaje"=>"Se guardaron los egresos", "datos" => $egreso], Response::HTTP_OK);
+        }
+        catch(Exception $e)
+        {
+            return response()->json(["error" => $e], Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
